31880da8b051cc8f249c87000c44f77bcb0f80ae
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2026-06-04 06:11:01 +0000

    [fern-generated] Update SDK
    Generated by Fern
    CLI Version: unknown
    Generators:
      - fernapi/fern-typescript-sdk: 3.48.1

--- src/api/types/DetectionLogDetectionObjectResponseBody.ts
+++ src/api/types/DetectionLogDetectionObjectResponseBody.ts
@@ -16,14 +16,14 @@ export interface DetectionLogDetectionObjectResponseBody {
     inCabAlertPlayed: boolean;
     /** Details on the associated safety event generated. Always returned. */
     safetyEvent: Samsara.DetectionLogSafetyEventObjectResponseBody[];
-    /** The label associated with the detection. Always returned.  Valid values: `acceleration`, `braking`, `crash`, `drowsy`, `eatingDrinking`, `edgeRailroadCrossingViolation`, `followingDistance`, `forwardCollisionWarning`, `genericDistraction`, `harshTurn`, `heavySpeeding`, `laneDeparture`, `lightSpeeding`, `maxSpeed`, `mobileUsage`, `moderateSpeeding`, `noSeatbelt`, `obstructedCamera`, `passenger`, `policyViolationMask`, `ranRedLight`, `rearCollisionWarning`, `reversing`, `rollingStop`, `rolloverProtection`, `severeSpeeding`, `smoking`, `speeding`, `unsafeParking`, `vehicleInBlindSpotWarning`, `vulnerableRoadUserCollisionWarning`, `yawControl` */
+    /** The label associated with the detection. Always returned.  Valid values: `acceleration`, `braking`, `crash`, `drowsy`, `eatingDrinking`, `edgeRailroadCrossingViolation`, `followingDistance`, `forwardCollisionWarning`, `genericDistraction`, `harshImpact`, `harshTurn`, `heavySpeeding`, `laneDeparture`, `lightSpeeding`, `maxSpeed`, `mobileUsage`, `moderateSpeeding`, `noSeatbelt`, `obstructedCamera`, `passenger`, `policyViolationMask`, `ranRedLight`, `rearCollisionWarning`, `reversing`, `rollingStop`, `rolloverProtection`, `severeSpeeding`, `smoking`, `speeding`, `unsafeParking`, `vehicleInBlindSpotWarning`, `vulnerableRoadUserCollisionWarning`, `yawControl` */
     triggerDetectionLabel: DetectionLogDetectionObjectResponseBody.TriggerDetectionLabel;
     /** Time the detection was updated in Samsara in UTC. RFC 3339 format. Always returned. */
     updatedAtTime: string;
 }
 
 export namespace DetectionLogDetectionObjectResponseBody {
-    /** The label associated with the detection. Always returned.  Valid values: `acceleration`, `braking`, `crash`, `drowsy`, `eatingDrinking`, `edgeRailroadCrossingViolation`, `followingDistance`, `forwardCollisionWarning`, `genericDistraction`, `harshTurn`, `heavySpeeding`, `laneDeparture`, `lightSpeeding`, `maxSpeed`, `mobileUsage`, `moderateSpeeding`, `noSeatbelt`, `obstructedCamera`, `passenger`, `policyViolationMask`, `ranRedLight`, `rearCollisionWarning`, `reversing`, `rollingStop`, `rolloverProtection`, `severeSpeeding`, `smoking`, `speeding`, `unsafeParking`, `vehicleInBlindSpotWarning`, `vulnerableRoadUserCollisionWarning`, `yawControl` */
+    /** The label associated with the detection. Always returned.  Valid values: `acceleration`, `braking`, `crash`, `drowsy`, `eatingDrinking`, `edgeRailroadCrossingViolation`, `followingDistance`, `forwardCollisionWarning`, `genericDistraction`, `harshImpact`, `harshTurn`, `heavySpeeding`, `laneDeparture`, `lightSpeeding`, `maxSpeed`, `mobileUsage`, `moderateSpeeding`, `noSeatbelt`, `obstructedCamera`, `passenger`, `policyViolationMask`, `ranRedLight`, `rearCollisionWarning`, `reversing`, `rollingStop`, `rolloverProtection`, `severeSpeeding`, `smoking`, `speeding`, `unsafeParking`, `vehicleInBlindSpotWarning`, `vulnerableRoadUserCollisionWarning`, `yawControl` */
     export const TriggerDetectionLabel = {
         Acceleration: "acceleration",
         Braking: "braking",
@@ -34,6 +34,7 @@ export namespace DetectionLogDetectionObjectResponseBody {
         FollowingDistance: "followingDistance",
         ForwardCollisionWarning: "forwardCollisionWarning",
         GenericDistraction: "genericDistraction",
+        HarshImpact: "harshImpact",
         HarshTurn: "harshTurn",
         HeavySpeeding: "heavySpeeding",
         LaneDeparture: "laneDeparture",
--- src/version.ts
+++ src/version.ts
@@ -1,1 +1,1 @@
-export const SDK_VERSION = "13.13.0";
+export const SDK_VERSION = "13.14.0";
